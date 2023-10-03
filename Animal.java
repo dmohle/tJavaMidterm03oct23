2e98b226548d5bed2a7448eed13e4bb649a9a009
dmohle <10038913+dmohle@users.noreply.github.com>
2023-10-03 12:22:15 -0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Animal {
+
+public Animal() {
+
+}
+    public Animal(String theName, String theSpecies) {
+        this.name = theName;
+        this.species = theSpecies;
+    }
+    String name;
+    String species;
+    float weight;
+    String animalID;
+    String origin;
+    String birthDay;
+    String gender;
+    String dateArrived;
+    Date dateArriveZoo;
+
+
+}
