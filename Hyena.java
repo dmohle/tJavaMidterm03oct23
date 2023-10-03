43f55238e53331317bfb3e448251d2658a9bb285
dmohle <10038913+dmohle@users.noreply.github.com>
2023-10-03 12:23:16 -0700

    Add files via upload

---
+++ Hyena.java
@@ -0,0 +1,12 @@
+public class Hyena extends Animal {
+    // Create a constructor for a name and species
+    public Hyena(String someVar01, String someVar02) {
+        super(someVar01,someVar02 );
+    }
+
+
+    public Hyena() {
+        super();
+    }
+
+}
